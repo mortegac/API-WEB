38243127febb574bee3f6915d107b78ddedf015d
mortegac@gmail.com <lvdp1980>
2016-05-23 15:18:05 -0300

    Agrego Modelos y controladores Personas

---
+++ WebAPI/Models/ClsPersonasModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class ClsPersonasModel
+    {
+        public int IdSocio { get; set; }
+        public string Nombres { get; set; }
+        public string ApePat { get; set; }
+        public string ApeMat { get; set; }
+
+
+
+    }
+}
